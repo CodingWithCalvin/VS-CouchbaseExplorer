9308cc5655e1ded970b23856f2594a1d8ff4cce3
Calvin A. Allen <calvin@codingwithcalvin.net>
2025-12-20 18:44:43 -0500

    fix(extension): resolve extension loading and improve menu placement (#315)

--- src/CodingWithCalvin.CouchbaseExplorer/CouchbaseExplorerPackage.cs
+++ src/CodingWithCalvin.CouchbaseExplorer/CouchbaseExplorerPackage.cs
@@ -2,6 +2,7 @@
 using System.Runtime.InteropServices;
 using System.Threading;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace CodingWithCalvin.CouchbaseExplorer
 {
@@ -9,7 +10,12 @@ namespace CodingWithCalvin.CouchbaseExplorer
     [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
     [Guid(PackageGuidString)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [ProvideToolWindow(typeof(CouchbaseExplorerWindow))]
+    [ProvideToolWindow(typeof(CouchbaseExplorerWindow),
+        Style = VsDockStyle.Linked,
+        Orientation = ToolWindowOrientation.Left,
+        Window = ToolWindowGuids.ServerExplorer)]
+    [ProvideAutoLoad(UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(UIContextGuids80.SolutionExists, PackageAutoLoadFlags.BackgroundLoad)]
     public sealed class CouchbaseExplorerPackage : AsyncPackage
     {
         public const string PackageGuidString = "ef261503-b2ae-4b90-8c86-0becd83348cc";
